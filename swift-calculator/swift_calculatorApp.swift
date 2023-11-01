fdcd7c92cc58f766eb936f175f0cd8f76c0e7672
amirk3321 <amirk3321@gmail.com>
2023-11-02 01:20:40 +0500

    Initial Commit

---
+++ swift-calculator/swift_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_calculatorApp.swift
+//  swift-calculator
+//
+//  Created by MA on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
